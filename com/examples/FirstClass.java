bb47505667e3070446cb4c3f26f5dd453c6450a8
NarendraNagam <narendranagam572@gmail.com>
2023-06-03 11:38:59 -0500

    Changes has been made related to object class example

---
+++ com/examples/FirstClass.java
@@ -0,0 +1,11 @@
+package com.examples;
+
+import com.examples.OOPS.Vehicle;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        Vehicle vehicle = new Vehicle();
+        vehicle.start();
+        vehicle.stop();
+    }
+}
